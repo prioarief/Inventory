883624d41d3d1069c1208e5e344120c645ecb783
prioarief <prioariefgunawangunawan@gmail.com>
2020-05-16 15:12:08 +0700

    set frontend

---
+++ application/views/index.php
@@ -0,0 +1,11 @@
+<div class="main-content">
+	<section class="section">
+		<div class="section-header">
+			<h1>Dashboard</h1>
+		</div>
+
+		<div class="section-body">
+			<h1 class="text-danger">Awowkwwko</h1>
+		</div>
+	</section>
+</div>
--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,17 +1,18 @@
 <!-- Main Content -->
-<div class="main-content">
+<!-- <div class="main-content">
 	<section class="section">
 		<div class="section-header">
-			<h1>Blank Page</h1>
+			<h1>Dashboard</h1>
 		</div>
 
 		<div class="section-body">
+			<h1 class="text-danger">Awowkwwko</h1>
 		</div>
 	</section>
-</div>
+</div> -->
 <footer class="main-footer">
 	<div class="footer-left">
-		Copyright &copy; 2018 <div class="bullet"></div> Design By <a href="https://nauval.in/">Muhamad Nauval Azhar</a>
+		Management Barang
 	</div>
 	<div class="footer-right">
 
@@ -21,21 +22,21 @@
 </div>
 
 <!-- General JS Scripts -->
-<script src="assets/modules/jquery.min.js"></script>
-<script src="assets/modules/popper.js"></script>
-<script src="assets/modules/tooltip.js"></script>
-<script src="assets/modules/bootstrap/js/bootstrap.min.js"></script>
-<script src="assets/modules/nicescroll/jquery.nicescroll.min.js"></script>
-<script src="assets/modules/moment.min.js"></script>
-<script src="assets/js/stisla.js"></script>
+<script src="<?= base_url() ?>assets/modules/jquery.min.js"></script>
+<script src="<?= base_url() ?>assets/modules/popper.js"></script>
+<script src="<?= base_url() ?>assets/modules/tooltip.js"></script>
+<script src="<?= base_url() ?>assets/modules/bootstrap/js/bootstrap.min.js"></script>
+<script src="<?= base_url() ?>assets/modules/nicescroll/jquery.nicescroll.min.js"></script>
+<script src="<?= base_url() ?>assets/modules/moment.min.js"></script>
+<script src="<?= base_url() ?>assets/js/stisla.js"></script>
 
 <!-- JS Libraies -->
 
 <!-- Page Specific JS File -->
 
 <!-- Template JS File -->
-<script src="assets/js/scripts.js"></script>
-<script src="assets/js/custom.js"></script>
+<script src="<?= base_url() ?>assets/js/scripts.js"></script>
+<script src="<?= base_url() ?>assets/js/custom.js"></script>
 </body>
 
 </html>
